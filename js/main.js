5a048d968fa8c938b307970add198b89a54c7f3a
Olivier <olivierchemla@gmail.com>
2018-12-03 21:20:22 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 // Trigger welcome modal on load.
-window.onload = function () {
-    $('#welcomeModal').modal('show')
-};
+// window.onload = function () {
+//     $('#welcomeModal').modal('show')
+// };
+
+// ScrollReveal().reveal('.reveal-left', {
+//     delay: 500,
+//     duration: 500,
+//     scale: 0.8,
+//     origin: 'left',
+//     distance: '300px',
+// });
+
+// ScrollReveal().reveal('.reveal-right', {
+//     delay: 500,
+//     duration: 500,
+//     scale: 0.8,
+//     origin: 'right',
+//     distance: '300px',
+// });
